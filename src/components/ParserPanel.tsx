6b410fccc222b755dbc78aec6d828cf31f468001
kazumatu981 <kazumatu981@gmail.com>
2024-09-12 08:30:33 +0900

    Change Mermaid to PrimeReact.

--- src/components/ParserPanel.tsx
+++ src/components/ParserPanel.tsx
@@ -1,37 +1,73 @@
 import { useEffect, useRef, useState } from 'react';
 import mermaid from 'mermaid';
-import { type ParserNode } from '../lib/parser/parser-node';
-
+import { BinaryNode, ParenNode, type ParserNode, SingleNode } from '../lib/parser/parser-node';
+import { Tree } from 'primereact/tree';
+import { TreeNode } from 'primereact/treenode';
 import { drawTree } from '../lib/util/node-to-mermaid';
 import { sha256Hash } from '../lib/util/sha256hash';
 
-export function ParserPanel({ parsedNode }: { parsedNode: ParserNode | undefined }) {
-    const ref = useRef<HTMLDivElement>(null);
-    const [src, setSrc] = useState<string | undefined>(undefined);
-    const [key, setKey] = useState<string>('');
+function parserNodeToTreeNode(parserNode: ParserNode): TreeNode {
+    if (parserNode.nodeType === 'paren') {
+        const node = parserNode as ParenNode;
+        return {
+            id: node.tokens[0].id,
+            key: node.tokens[0].id,
+            label: '()',
+            data: node,
+            children: [parserNodeToTreeNode(node.childRoot)],
+        };
+    } else if (parserNode.nodeType === 'binary') {
+        const node = parserNode as BinaryNode;
+        return {
+            id: node.tokens[0].id,
+            key: node.tokens[0].id,
+            label: node.operator,
+            data: node,
+            children: [parserNodeToTreeNode(node.left), parserNodeToTreeNode(node.right)],
+        };
+    } else if (parserNode.nodeType === 'single') {
+        const node = parserNode as SingleNode;
+        return {
+            id: node.tokens[0].id,
+            key: node.tokens[0].id,
+            label: node.isNegative ? `-${node.value}` : node.value,
+            data: node,
+        };
+    }
+    throw new Error('not implemented');
+}
 
-    useEffect(() => {
-        console.log('update mermaid');
-        if (parsedNode) {
-            const mermaidSrc = drawTree(parsedNode);
-            sha256Hash(mermaidSrc).then((hash) => {
-                setSrc(mermaidSrc);
-                setKey(hash);
-            });
-        }
-    }, [parsedNode]);
-
-    useEffect(() => {
-        if (src) {
-            mermaid.init({}, ref.current === null ? undefined : ref.current);
-        }
-    }, [src]);
-
-    return src && parsedNode ? (
-        <div className="mermaid" ref={ref} key={key}>
-            {src}
+function nodeTemplate(node: TreeNode): JSX.Element {
+    const classNames = [
+        'flex',
+        'flex-row',
+        'gap-1',
+        'border-1',
+        'p-2',
+        'border-round-md',
+        'align-items-center',
+        'justify-content-center',
+        'shadow-7',
+    ];
+    if ((node.data as ParserNode).nodeType === 'binary') {
+        classNames.push('bg-teal-100');
+    }
+    if ((node.data as ParserNode).nodeType === 'paren') {
+        classNames.push('bg-orange-100');
+    }
+    return (
+        <div className={classNames.join(' ')}>
+            <div className="text-xl">{node.label}</div>
+            <div className="ml-3">
+                <small>
+                    <i className="pi pi-map-marker text-xs"></i>
+                    {(node.data as ParserNode).tokens[0].position}
+                </small>
+            </div>
         </div>
-    ) : (
-        <div key={key} />
     );
+}
+
+export function ParserPanel({ parsedNode }: { parsedNode: ParserNode | undefined }) {
+    return parsedNode ? <Tree value={[parserNodeToTreeNode(parsedNode)]} nodeTemplate={nodeTemplate} /> : <></>;
 }
