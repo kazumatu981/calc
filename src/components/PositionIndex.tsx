ad591445b19366e252d62fab985cef42347ac859
kazumatu981 <kazumatu981@gmail.com>
2024-09-11 17:20:51 +0900

    hot fix.

--- src/components/PositionIndex.tsx
+++ src/components/PositionIndex.tsx
@@ -1,13 +1,16 @@
 export function PositionIndex({ expression }: { expression: string }): JSX.Element {
-    const header = (
-        <div className="flex flex-column border-1">
-            <div className="flex align-items-center justify-content-center border-bottom-1 p-1">{'expression'}</div>
-            <div className="flex align-items-center justify-content-center border-bottom-1 p-1 bg-gray-300">
-                <i className="pi pi-map-marker text-xs"></i>
-                {'position'}
+    const header =
+        expression.length === 0 ? (
+            <></>
+        ) : (
+            <div className="flex flex-column border-1">
+                <div className="flex align-items-center justify-content-center border-bottom-1 p-1">{'character'}</div>
+                <div className="flex align-items-center justify-content-center border-bottom-1 p-1 bg-gray-300">
+                    <i className="pi pi-map-marker text-xs"></i>
+                    {'position'}
+                </div>
             </div>
-        </div>
-    );
+        );
     const charArray = expression.split('').map((charItem, index) => {
         return (
             <div className="flex flex-column border-1">
